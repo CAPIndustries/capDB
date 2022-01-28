17c6a3614349c429ada8b19590e563bbf4fe1ff3
Krishna Solanki <krishna.solanki@mail.utoronto.ca>
2022-01-27 23:16:45 -0500

    Added code to make queue thread safe, added read semaphore to allow concurrent reads - need to remove debug statements

--- src/app_kvServer/ClientConnection.java
+++ src/app_kvServer/ClientConnection.java
@@ -12,29 +12,30 @@ import shared.messages.TextMessage;
 import shared.messages.KVMessage.StatusType;
 
 /**
- * Represents a connection end point for a particular client that is 
- * connected to the server. This class is responsible for message reception 
- * and sending. 
- * The class also implements the echo functionality. Thus whenever a message 
+ * Represents a connection end point for a particular client that is
+ * connected to the server. This class is responsible for message reception
+ * and sending.
+ * The class also implements the echo functionality. Thus whenever a message
  * is received it is going to be echoed back to the client.
  */
 public class ClientConnection implements Runnable {
 
 	private static Logger logger = Logger.getRootLogger();
-	
+
 	private boolean isOpen;
 	private static final int BUFFER_SIZE = 1024;
 	private static final int DROP_SIZE = 128 * BUFFER_SIZE;
 	private static final char LINE_FEED = 0x0A;
 	private static final char RETURN = 0x0D;
-	
+
 	private Socket clientSocket;
 	private InputStream input;
 	private OutputStream output;
 	KVServer server;
-	
+
 	/**
 	 * Constructs a new CientConnection object for a given TCP socket.
+	 * 
 	 * @param clientSocket the Socket object for the client connection.
 	 */
 	public ClientConnection(Socket clientSocket, KVServer server) {
@@ -42,17 +43,17 @@ public class ClientConnection implements Runnable {
 		this.server = server;
 		this.isOpen = true;
 	}
-	
+
 	/**
-	 * Initializes and starts the client connection. 
+	 * Initializes and starts the client connection.
 	 * Loops until the connection is closed or aborted by the client.
 	 */
 	public void run() {
 		try {
 			output = clientSocket.getOutputStream();
 			input = clientSocket.getInputStream();
-			
-			while(isOpen) {
+
+			while (isOpen) {
 				try {
 					TextMessage latestMsg = receiveMessage();
 					switch (latestMsg.getStatus()) {
@@ -71,23 +72,26 @@ public class ClientConnection implements Runnable {
 							break;
 						default:
 							// TODO: Send an error to the user, probably need another StatusType value
+							System.out.println("OH NOOOOOOO");
 							System.out.println("Status:" + latestMsg.getStatus());
 							break;
 					}
-					
-				/* connection either terminated by the client or lost due to 
-				 * network problems*/	
+
+					/*
+					 * connection either terminated by the client or lost due to
+					 * network problems
+					 */
 				} catch (IOException ioe) {
 					logger.error("Error! Connection lost!");
 					isOpen = false;
-				}				
+				}
 			}
-			
+
 		} catch (IOException ioe) {
 			logger.error("Error! Connection could not be established!", ioe);
-			
+
 		} finally {
-			
+
 			try {
 				if (clientSocket != null) {
 					input.close();
@@ -102,17 +106,17 @@ public class ClientConnection implements Runnable {
 
 	private TextMessage putKV(String key, String value) {
 		TextMessage res;
-		
+
 		try {
 			StatusType putStatus = server.inStorage(key) ? StatusType.PUT_UPDATE : StatusType.PUT_SUCCESS;
 			server.putKV(key, value);
 
 			res = new TextMessage(key, value, putStatus);
 		} catch (Exception e) {
-			//TODO: handle exception
+			// TODO: handle exception
 			res = new TextMessage(key, value, StatusType.PUT_ERROR);
 		}
-		
+
 		return res;
 	}
 
@@ -124,43 +128,45 @@ public class ClientConnection implements Runnable {
 			value = server.getKV(key);
 			res = new TextMessage(key, value, StatusType.GET_SUCCESS);
 		} catch (Exception e) {
-			//TODO: handle exception
+			// TODO: handle exception
+			System.out.println("Err:" + e.getMessage());
 			res = new TextMessage(key, e.getMessage(), StatusType.GET_ERROR);
 		}
-		
+
 		return res;
 	}
-	
+
 	/**
 	 * Method sends a TextMessage using this socket.
+	 * 
 	 * @param msg the message that is to be sent.
-	 * @throws IOException some I/O error regarding the output stream 
+	 * @throws IOException some I/O error regarding the output stream
 	 */
 	public void sendMessage(TextMessage msg) throws IOException {
 		byte[] msgBytes = msg.getMsgBytes();
 		output.write(msgBytes, 0, msgBytes.length);
 		output.flush();
-		// logger.info("SEND \t<" 
-		// 		+ clientSocket.getInetAddress().getHostAddress() + ":" 
-		// 		+ clientSocket.getPort() + ">: '" 
-		// 		+ msg.getMsg() +"'");
-    }
-	
+		// logger.info("SEND \t<"
+		// + clientSocket.getInetAddress().getHostAddress() + ":"
+		// + clientSocket.getPort() + ">: '"
+		// + msg.getMsg() +"'");
+	}
+
 	private TextMessage receiveMessage() throws IOException {
 		int index = 0;
 		byte[] msgBytes = null, tmp = null;
 		byte[] bufferBytes = new byte[BUFFER_SIZE];
-		
+
 		/* read first char from stream */
-		byte read = (byte) input.read();	
+		byte read = (byte) input.read();
 		boolean reading = true;
 
 		System.out.println("First read:" + read);
 
-		while (read != LINE_FEED && read != -1 && reading) {/* LF, error, drop*/
+		while (read != LINE_FEED && read != -1 && reading) {/* LF, error, drop */
 			/* if buffer filled, copy to msg array */
-			if(index == BUFFER_SIZE) {
-				if(msgBytes == null){
+			if (index == BUFFER_SIZE) {
+				if (msgBytes == null) {
 					tmp = new byte[BUFFER_SIZE];
 					System.arraycopy(bufferBytes, 0, tmp, 0, BUFFER_SIZE);
 				} else {
@@ -173,46 +179,51 @@ public class ClientConnection implements Runnable {
 				msgBytes = tmp;
 				bufferBytes = new byte[BUFFER_SIZE];
 				index = 0;
-			} 
-			
+			}
+
 			/* only read valid characters, i.e. letters and numbers */
 			// if((read > 31 && read < 127)) {
-				bufferBytes[index] = read;
-				index++;
+			bufferBytes[index] = read;
+			index++;
 			// }
-			
+
 			/* stop reading is DROP_SIZE is reached */
-			if(msgBytes != null && msgBytes.length + index >= DROP_SIZE) {
+			if (msgBytes != null && msgBytes.length + index >= DROP_SIZE) {
 				reading = false;
 			}
-			
+
 			/* read next char from stream */
 			read = (byte) input.read();
 		}
 
 		System.out.println("Last read:" + read);
 		// if (read == -1) {
-		// 	System.out.println("Its -1");
-		// 	isOpen = false;
+		// System.out.println("Its -1");
+		// isOpen = false;
 		// }
-		
-		if(msgBytes == null){
+
+		if (msgBytes == null) {
+			System.out.println("null message");
 			tmp = new byte[index];
 			System.arraycopy(bufferBytes, 0, tmp, 0, index);
+			System.out.println("done null message");
 		} else {
+			System.out.println("not null message");
 			tmp = new byte[msgBytes.length + index];
 			System.arraycopy(msgBytes, 0, tmp, 0, msgBytes.length);
 			System.arraycopy(bufferBytes, 0, tmp, msgBytes.length, index);
+			System.out.println("done not null message");
 		}
-		
+
 		msgBytes = tmp;
-		
+		System.out.println("make into text message");
 		/* build final result */
 		TextMessage msg = new TextMessage(msgBytes);
-		// logger.info("RECEIVE \t<" 
-		// 		+ clientSocket.getInetAddress().getHostAddress() + ":" 
-		// 		+ clientSocket.getPort() + ">: '" 
-		// 		+ msg.getMsg().trim() + "'");
+		// logger.info("RECEIVE \t<"
+		// + clientSocket.getInetAddress().getHostAddress() + ":"
+		// + clientSocket.getPort() + ">: '"
+		// + msg.getMsg().trim() + "'");
+		System.out.println("DONE reading");
 		return msg;
 	}
 }
--- src/app_kvServer/KVServer.java
+++ src/app_kvServer/KVServer.java
@@ -13,17 +13,19 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.concurrent.*;
+import java.util.function.IntBinaryOperator;
+import java.util.Iterator;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
 import logger.LogSetup;
 
-
 public class KVServer implements IKVServer {
 
 	private static Logger logger = Logger.getRootLogger();
 	private static final String STORAGE_DIRECTORY = "storage/";
+	private static final Integer MAX_READS = 100;
 	private int port;
 	private int cacheSize;
 	private CacheStrategy strategy;
@@ -32,19 +34,22 @@ public class KVServer implements IKVServer {
 	File storageDirectory = new File(STORAGE_DIRECTORY);
 
 	// true = write in progress (locked) and false = data is accessible
-	ConcurrentMap<String, Queue<Integer>> fileList = new ConcurrentHashMap<String, Queue<Integer>>();
+	ConcurrentMap<String, Queue<Integer[]>> fileList = new ConcurrentHashMap<String, Queue<Integer[]>>();
 
+	// semaphore map to allow multiple reads
+	ConcurrentMap<String, Semaphore> readMap = new ConcurrentHashMap<String, Semaphore>();
 
-	
 	/**
 	 * Start KV Server at given port
-	 * @param port given port for storage server to operate
+	 * 
+	 * @param port      given port for storage server to operate
 	 * @param cacheSize specifies how many key-value pairs the server is allowed
-	 *           to keep in-memory
-	 * @param strategy specifies the cache replacement strategy in case the cache
-	 *           is full and there is a GET- or PUT-request on a key that is
-	 *           currently not contained in the cache. Options are "FIFO", "LRU",
-	 *           and "LFU".
+	 *                  to keep in-memory
+	 * @param strategy  specifies the cache replacement strategy in case the cache
+	 *                  is full and there is a GET- or PUT-request on a key that is
+	 *                  currently not contained in the cache. Options are "FIFO",
+	 *                  "LRU",
+	 *                  and "LFU".
 	 */
 	public KVServer(int port, int cacheSize, CacheStrategy strategy) {
 		this.port = port;
@@ -52,46 +57,46 @@ public class KVServer implements IKVServer {
 		this.strategy = strategy;
 		this.run();
 	}
-	
+
 	@Override
-	public int getPort(){
+	public int getPort() {
 		return this.port;
 	}
 
 	@Override
-    public String getHostname(){
+	public String getHostname() {
 		return serverSocket.getInetAddress().getHostName();
 	}
 
 	@Override
-    public CacheStrategy getCacheStrategy(){
+	public CacheStrategy getCacheStrategy() {
 		return this.strategy;
 	}
 
 	@Override
-    public int getCacheSize(){
+	public int getCacheSize() {
 		return this.cacheSize;
 	}
 
 	@Override
-    public boolean inCache(String key){
+	public boolean inCache(String key) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-    public void clearCache(){
+	public void clearCache() {
 		// TODO Auto-generated method stub
 	}
 
 	@Override
-    public boolean inStorage(String key){
+	public boolean inStorage(String key) {
 		// TODO: See if someone is writing to fileList first
 		return fileList.containsKey(key);
 	}
 
 	@Override
-    public void clearStorage(){
+	public void clearStorage() {
 		fileList.clear();
 		File[] allContents = storageDirectory.listFiles();
 		if (allContents != null) {
@@ -102,41 +107,72 @@ public class KVServer implements IKVServer {
 	}
 
 	@Override
-    public String getKV(String key) throws Exception {
+	public String getKV(String key) throws Exception {
+		System.out.println("GET this key: " + key);
 		// Check if key exists
 		if (!inStorage(key)) {
 			throw new Exception("Key does not exist");
 		} else {
-			// fileList.put(key, true);	
+			// fileList.put(key, true);
+			// TODO - swap ints with ENUM - write - 1 and read -0
+			System.out.println("looks chill");
+			Integer[] node = { (int) Thread.currentThread().getId(), 0 };
+			fileList.get(key).add(node);
+			System.out.println("looks okay");
+
+			while (fileList.get(key).peek() != null && fileList.get(key).peek()[1] != 1) {
+				try {
+					readMap.get(key).acquire();
+					System.out.println("file list1: " + fileList.get(key).peek());
+					fileList.get(key).remove();
+					if (fileList.get(key).peek() == null)
+						break;
+					System.out.println("file list2: " + fileList.get(key).peek());
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			System.out.println("over here");
+			System.out.println("cnt:" + readMap.get(key).availablePermits());
 			File file = new File(STORAGE_DIRECTORY + key);
-			StringBuilder fileContents = new StringBuilder((int)file.length());        
+			StringBuilder fileContents = new StringBuilder((int) file.length());
 			String value;
-			
+
 			try (Scanner scanner = new Scanner(file)) {
 				while (scanner.hasNextLine()) {
 					fileContents.append(scanner.nextLine() + System.lineSeparator());
 				}
 				// fileList.put(key, false);
+				if (fileList.get(key).peek() != null)
+					fileList.get(key).remove();
+				readMap.get(key).release();
 				return fileContents.toString().trim();
+			} catch (Error e) {
+				if (fileList.get(key).peek() != null)
+					fileList.get(key).remove();
+				readMap.get(key).release();
+				e.printStackTrace();
 			}
-			
 		}
+		return "Error";
 	}
 
 	@Override
-    public void putKV(String key, String value) throws Exception {
+	public void putKV(String key, String value) throws Exception {
 		try {
-			// create new list if new 
-			if (!inStorage(key)) {
-				fileList.put(key, new LinkedList<Integer>());
-			}
-			
-			// add thread to back of list for this key 
-			fileList.get(key).add((int)Thread.currentThread().getId());
+			fileList.putIfAbsent(key, new ConcurrentLinkedQueue<Integer[]>());
+			readMap.putIfAbsent(key, new Semaphore(MAX_READS));
 
-			//  wait until threads turn 
-			do {} while(fileList.get(key).peek() != ((int)Thread.currentThread().getId()));
-			
+			// add thread to back of list for this key - add is thread safe
+			Integer[] node = { (int) Thread.currentThread().getId(), 1 };
+			fileList.get(key).add(node);
+
+			// wait until threads turn and no one is reading
+			do {
+			} while (fileList.get(key).peek() != null
+					&& fileList.get(key).peek()[0] != ((int) Thread.currentThread().getId())
+					&& readMap.get(key).availablePermits() != MAX_READS);
+
 			if (value.equals("null")) {
 				// Delete the key
 				// TODO: Get a lock on the fileList since I'm updating/writing to it
@@ -153,54 +189,54 @@ public class KVServer implements IKVServer {
 					FileWriter myWriter = new FileWriter("storage/" + key);
 					myWriter.write(value);
 					myWriter.close();
-				  } catch (IOException e) {
+				} catch (IOException e) {
 					e.printStackTrace();
-				  }
+				}
 				// fileList.put(key, false);
 			}
 
-			// remove the top item from the list for this key 
-			fileList.get(key).remove();
+			// remove the top item from the list for this key
+			if (fileList.get(key).peek() != null)
+				fileList.get(key).remove();
 
 		} catch (Exception e) {
-			//TODO: handle exception
+			// TODO: handle exception
 		}
 	}
 
 	@Override
-    public void run(){
+	public void run() {
 		running = initializeServer();
-        
-        if(serverSocket != null) {
-	        while(isRunning()){
-	            try {
-	                Socket client = serverSocket.accept();                
-	                ClientConnection connection = 
-	                		new ClientConnection(client, this);
-	                new Thread(connection).start();
-	                
-	                logger.info("Connected to " 
-	                		+ client.getInetAddress().getHostName() 
-	                		+  " on port " + client.getPort());
-	            } catch (IOException e) {
-	            	logger.error("Error! " +
-	            			"Unable to establish connection. \n", e);
-	            }
-	        }
-        }
-        logger.info("Server stopped.");
+
+		if (serverSocket != null) {
+			while (isRunning()) {
+				try {
+					Socket client = serverSocket.accept();
+					ClientConnection connection = new ClientConnection(client, this);
+					new Thread(connection).start();
+
+					logger.info("Connected to "
+							+ client.getInetAddress().getHostName()
+							+ " on port " + client.getPort());
+				} catch (IOException e) {
+					logger.error("Error! " +
+							"Unable to establish connection. \n", e);
+				}
+			}
+		}
+		logger.info("Server stopped.");
 	}
 
 	// TODO: Difference between kill and close?
 	@Override
-    public void kill(){
+	public void kill() {
 		// TODO Auto-generated method stub
 	}
 
 	@Override
-    public void close(){
+	public void close() {
 		running = false;
-        try {
+		try {
 			serverSocket.close();
 		} catch (IOException e) {
 			logger.error("Error! " +
@@ -209,18 +245,19 @@ public class KVServer implements IKVServer {
 	}
 
 	/**
-     * Main entry point for the KVServer application. 
-     * @param args contains the program's input args (here for signature purposes)
-     */
-    public static void main(String[] args) {
-    	try {
+	 * Main entry point for the KVServer application.
+	 * 
+	 * @param args contains the program's input args (here for signature purposes)
+	 */
+	public static void main(String[] args) {
+		try {
 			new LogSetup("logs/server.log", Level.ALL);
-			if(args.length != 1) {
+			if (args.length != 1) {
 				System.out.println("Error! Invalid number of arguments!");
 				System.out.println("Usage: Server <port>!");
 			} else {
 				int port = Integer.parseInt(args[0]);
-				// No need to use the run method here since the contructor is supposed to 
+				// No need to use the run method here since the contructor is supposed to
 				// start the server on its own
 				// TODO: Allow passing additional arguments from the command line:
 				new KVServer(port, 0, CacheStrategy.None);
@@ -234,45 +271,46 @@ public class KVServer implements IKVServer {
 			System.out.println("Usage: Server <port>!");
 			System.exit(1);
 		}
-    }
+	}
 
 	private boolean initializeServer() {
-    	logger.info("Initialize server ...");
+		logger.info("Initialize server ...");
 		initializeStorage();
-    	try {
-            serverSocket = new ServerSocket(port);
-            logger.info("Server listening on port: " 
-            		+ serverSocket.getLocalPort());    
-            return true;
-        
-        } catch (IOException e) {
-        	logger.error("Error! Cannot open server socket:");
-            if(e instanceof BindException){
-            	logger.error("Port " + port + " is already bound!");
-            }
-            return false;
-        }
-    }
+		try {
+			serverSocket = new ServerSocket(port);
+			logger.info("Server listening on port: "
+					+ serverSocket.getLocalPort());
+			return true;
+
+		} catch (IOException e) {
+			logger.error("Error! Cannot open server socket:");
+			if (e instanceof BindException) {
+				logger.error("Port " + port + " is already bound!");
+			}
+			return false;
+		}
+	}
 
 	private void initializeStorage() {
 		logger.info("Initialize storage ...");
 		// Ensure storage directory exists
-		if (!storageDirectory.exists()){
+		if (!storageDirectory.exists()) {
 			storageDirectory.mkdir();
 		} else {
 			// Load all the data
 			File[] listOfFiles = storageDirectory.listFiles();
 			for (int i = 0; i < listOfFiles.length; i++) {
 				if (listOfFiles[i].isFile()) {
-					fileList.put(listOfFiles[i].getName(), new LinkedList<Integer>());
-				} 
+					readMap.put(listOfFiles[i].getName(), new Semaphore(MAX_READS));
+					fileList.put(listOfFiles[i].getName(), new ConcurrentLinkedQueue<Integer[]>());
+				}
 			}
 		}
 	}
 
 	private boolean isRunning() {
-        return this.running;
-    }
+		return this.running;
+	}
 
 	public void setRunning(boolean run) {
 		this.running = run;
