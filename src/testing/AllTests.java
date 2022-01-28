88b7921daa54bd61789fc3f9bcd1f1a177574365
Krishna Solanki <krishna.solanki@mail.utoronto.ca>
2022-01-28 13:42:42 -0500

    Fixed test to spawn server in different thread

--- src/testing/AllTests.java
+++ src/testing/AllTests.java
@@ -10,27 +10,31 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 import logger.LogSetup;
 
-
 public class AllTests {
 
 	static {
 		try {
 			new LogSetup("logs/testing/test.log", Level.ERROR);
-			new KVServer(50000, 10, CacheStrategy.None);
+			Runnable server = new Runnable() {
+				@Override
+				public void run() {
+					new KVServer(50000, 10, CacheStrategy.None);
+				}
+			};
+			new Thread(server).start();
 			// Originally was:
 			// new KVServer(50000, 10, CacheStrategy.FIFO);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	
+
 	public static Test suite() {
 		TestSuite clientSuite = new TestSuite("Basic Storage ServerTest-Suite");
 		clientSuite.addTestSuite(ConnectionTest.class);
-		clientSuite.addTestSuite(InteractionTest.class); 
-		clientSuite.addTestSuite(AdditionalTest.class); 
+		clientSuite.addTestSuite(InteractionTest.class);
+		clientSuite.addTestSuite(AdditionalTest.class);
 		return clientSuite;
 	}
-	
+
 }
